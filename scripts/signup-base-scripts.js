e8cf598293df6f24bea472544b05e4247ccf5424
f3xy1 <yx3fnd@gmail.com>
2025-03-31 03:03:53 +0500

    Первая версия

---
+++ scripts/signup-base-scripts.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.toggle-password').forEach(button => {
+    button.addEventListener('click', function() {
+        const input = this.previousElementSibling;
+        const icon = this.querySelector('i');
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.replace('fa-eye-slash', 'fa-eye');
+        } else {
+            input.type = 'password';
+            icon.classList.replace('fa-eye', 'fa-eye-slash');
+        }
+    });
+});
